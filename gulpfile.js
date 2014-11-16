ca470fcac90c0346960d3105615336e1a492553a
Daniel Bergey <bergey@alum.mit.edu>
2014-11-16 21:02:59 +0000

    browserify instead of requirejs
    setup gulp to rebuild, watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,44 +3,65 @@
 "use strict";
 
 var gulp = require("gulp");
-var rjs = require("gulp-requirejs");
-var bowerRequireJS = require("bower-requirejs");
+var gutil = require("gulp-util");
 var uglify = require("gulp-uglify");
 var ghPages = require("gulp-gh-pages");
 var del = require("del");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var watchify = require("watchify");
 
-gulp.task("requirejs", ["clean"], function() {
-    rjs({
-        baseUrl: "js",
-        mainConfigFile: "js/rjs.js",
-        // baseUrl: "js/rjs.js",
-        out: "js/rjs.js",
-        name: "sdp/main",
-    })
-        // .pipe(uglify())
-        .pipe(gulp.dest("dist"));
+var watchOrBrowserify = function(watch) { // false for distribution, true for dev
+    return function() {
+        var bundler = browserify({
+            cache: {},
+            packageCache: {},
+            fullPaths: true,
+            entries: ["./js/main.js"],
+            debug: true,
+            outputName: "js/school-districts.js"
+        });
+
+        // Optionally, you can apply transforms
+        // and other configuration options on the
+        // bundler just as you would with browserify
+        // bundler.transform("brfs");
+
+        function rebundle() {
+            return bundler.bundle()
+            // log errors if they happen
+                .on("error", gutil.log.bind(gutil, "Browserify Error"))
+                .pipe(source("js/school-districts.js"))
+                .pipe(gulp.dest("./"));
+        }
+
+        if (watch) {
+            bundler = watchify(bundler);
+            bundler.on("update", rebundle);
+        }
+
+        return rebundle();
+    };
+};
+
+// synonym for watchify, until I have CSS or something to compile
+gulp.task("watch", ["watchify"]);
+
+gulp.task("browserify", watchOrBrowserify(false));
+
+gulp.task("watchify", watchOrBrowserify(true));
+
+gulp.task("clean", function(cb) {
+    // delete everything under dist/ but leave the directory
+    del("dist/?**", cb);
 });
 
-gulp.task("copy", ["clean"], function() {
+gulp.task("dist", ["clean", "browserify"], function() {
     gulp.src("index.html").pipe(gulp.dest("dist"));
     gulp.src("css/*.css").pipe(gulp.dest("dist/css"));
     gulp.src("data/*").pipe(gulp.dest("dist/data"));
+    gulp.src("js/school-districts.js").pipe(gulp.dest("dist/js"));
 });
-
-gulp.task("bower", function() {
-    bowerRequireJS({
-        baseUrl: "js",
-        config: "js/rjs.js",
-        transitive: true
-    });
-});
-
-gulp.task("clean", function(cb) {
-    del("dist/**", cb);
-    // cb(null); // null signals completion without error
-});
-
-gulp.task("dist", ["copy", "requirejs"]);
 
 gulp.task("gh-pages", ["dist"], function() {
     return gulp.src("dist/**/*")
